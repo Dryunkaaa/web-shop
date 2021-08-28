b0c2d096cc52e25bb2be83fc8f15aea1498b3100
Andrey <dryunkaaa@gmail.com>
2021-08-28 08:25:44 +0300

    Added handler of static resources.

--- src/main/java/com/shop/config/WebMvcConfig.java
+++ src/main/java/com/shop/config/WebMvcConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.SpringTemplateEngine;
@@ -26,6 +27,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
         templateResolver.setApplicationContext(context);
         templateResolver.setPrefix("/WEB-INF/views/");
         templateResolver.setSuffix(".html");
+
         return templateResolver;
     }
 
@@ -34,6 +36,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
         SpringTemplateEngine templateEngine = new SpringTemplateEngine();
         templateEngine.setTemplateResolver(templateResolver());
         templateEngine.setEnableSpringELCompiler(true);
+
         return templateEngine;
     }
 
@@ -42,5 +45,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
         ThymeleafViewResolver resolver = new ThymeleafViewResolver();
         resolver.setTemplateEngine(templateEngine());
         registry.viewResolver(resolver);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/", "/resources/images/");
     }
 }
--- src/main/java/com/shop/controller/IndexController.java
+++ src/main/java/com/shop/controller/IndexController.java
@@ -1,13 +1,19 @@
 package com.shop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Controller
 public class IndexController {
 
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/mm/yyyy HH:mm:ss");
+        model.addAttribute("localDate", dateFormat.format(new Date()));
         return "index";
     }
 }
